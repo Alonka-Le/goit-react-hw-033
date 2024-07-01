18b921db7aff9a8a53bdadbe45809a945c814ce3
Alonka-Le <alyona.sholohowa120@gmail.com>
2024-07-01 17:41:42 +0200

    components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import contact from "../contact.json";
+import contact from "../../contact.json";
 import ContactForm from "../ContactForm/ContactForm";
 import SearchBox from "../SearchBox/SearchBox";
 import ContactList from "../ContactList/ContactList";
